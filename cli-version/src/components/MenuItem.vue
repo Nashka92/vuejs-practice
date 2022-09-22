f80516b0ad94c82c30e13924362cf9b6d0867936
anna raach <anna.raach@anywr-group.com>
2022-09-22 02:31:48 +0200

    Practice vueJS V01

---
+++ cli-version/src/components/MenuItem.vue
@@ -0,0 +1,34 @@
+<script>
+	//ici j'exporte mon component que je vais appeler MenuItem
+	// avec ses propriétés
+export default {
+	name: "MenuItem",
+	props: ["addToShoppingCart", "image", "inStock", "name", "quantity"]
+}
+</script>
+
+<!-- Le template sera l'affichage de mon component -->
+<template>
+	<div class="menu-item">
+		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
+		<div>
+			<h3>{{ name }}</h3>
+			<!-- Condition s'il est en stock afficher "en stock",
+				sinon afficher "rupture" -->
+			<p v-if="inStock">En stock</p>
+			<p v-else>En rupture de stock</p>
+			<div>
+				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
+				<!-- Ici c'est ce qui permet de mettre à jour la quantité que l'user
+				va mettre dans son panier, j'utilise propriété "model" pour mettre à jour -->
+				<input v-model.number="quantity" id="add-item-quantity" type="number" />
+				<!-- propriété v-on (@click) pour augmenter le total du panier -->
+				<button @click="addToShoppingCart(quantity)">
+					Ajouter au panier
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style></style>
